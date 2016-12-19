35d277b22ab257d65f5eb33f812871643bf449b6
Ostap <prickly4528@gmail.com>
2016-12-20 00:33:48 +0200

    add ajax form
    add animated css/add wow js

---
+++ js/form.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('#submit').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/ostapukrainian@ukr.net",
+            method: "POST",
+            data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                message: $('#message').val();
+            },
+            dataType: "json"
+        })
+        .done(function(){
+          $('form').html('<h1>Thank you!</h1>');  
+        });
+    });
+});
